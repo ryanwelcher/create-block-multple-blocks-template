d6ed453d92572f8daa5d7bfa9172aed733c9bf6e
Ryan Welcher <ryan.welcher@automattic.com>
2021-10-27 13:12:54 -0400

    Add defaults for styles and scripts.

--- inner-template/index.js
+++ inner-template/index.js
@@ -3,6 +3,9 @@ const { join } = require( 'path' );
 module.exports = {
     defaultValues: {
         wpScripts: false,
+        editorScript: "file:../../../../build/{{slug}}.js",
+        editorStyle: "file:../../../../build/{{slug}}.css",
+        style: "file:../../../../build/{{slug}}.css",
     },
     templatesPath: join( __dirname, 'templates' ),
 };
